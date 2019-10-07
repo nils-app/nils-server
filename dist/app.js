32a391cf55e4ff93313d45bc447ad36f41b7fbfb
Alejandro U. Alvarez <aluralma@gmail.com>
2019-10-07 13:28:05 +0100

    cleanup

--- dist/app.js
+++ dist/app.js
@@ -11,8 +11,6 @@ const passport_1 = __importDefault(require("passport"));
 const auth_1 = __importDefault(require("./endpoints/auth"));
 const status_1 = __importDefault(require("./endpoints/status"));
 const users_1 = __importDefault(require("./endpoints/users"));
-// API keys and Passport configuration
-// import * as passportConfig from "./config/passport";
 // Create Express server
 const app = express_1.default();
 // Express configuration
@@ -20,30 +18,10 @@ app.set('port', process.env.PORT || 3000);
 app.use(compression_1.default());
 app.use(body_parser_1.default.json());
 app.use(body_parser_1.default.urlencoded({ extended: true }));
-// app.use(passport.initialize());
-// app.use(passport.session());
 app.use(lusca_1.default.xframe('SAMEORIGIN'));
 app.use(lusca_1.default.xssProtection(true));
 app.use(passport_1.default.initialize());
 app.use(passport_1.default.session());
-// app.use((req, res, next) => {
-//     res.locals.user = req.user;
-//     next();
-// });
-// app.use((req, res, next) => {
-//     // After successful login, redirect back to the intended page
-//     if (!req.user &&
-//     req.path !== "/login" &&
-//     req.path !== "/signup" &&
-//     !req.path.match(/^\/auth/) &&
-//     !req.path.match(/\./)) {
-//         req.session.returnTo = req.path;
-//     } else if (req.user &&
-//     req.path == "/account") {
-//         req.session.returnTo = req.path;
-//     }
-//     next();
-// });
 /**
  * Primary app routes.
  */
@@ -51,17 +29,5 @@ app.use('/users', users_1.default);
 app.use('/auth', auth_1.default);
 // This endpoint must be the last one
 app.get('/', status_1.default(app));
-// /**
-//  * API examples routes.
-//  */
-// app.get("/api", apiController.getApi);
-// app.get("/api/facebook", passportConfig.isAuthenticated, passportConfig.isAuthorized, apiController.getFacebook);
-// /**
-//  * OAuth authentication routes. (Sign in)
-//  */
-// app.get("/auth/facebook", passport.authenticate("facebook", { scope: ["email", "public_profile"] }));
-// app.get("/auth/facebook/callback", passport.authenticate("facebook", { failureRedirect: "/login" }), (req, res) => {
-//     res.redirect(req.session.returnTo || "/");
-// });
 exports.default = app;
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -8,43 +8,22 @@ import auth from './endpoints/auth'
 import status from './endpoints/status'
 import users from './endpoints/users'
 
-// API keys and Passport configuration
-// import * as passportConfig from "./config/passport";
-
 // Create Express server
 const app = express()
 
 // Express configuration
 app.set('port', process.env.PORT || 3000)
+
 app.use(compression())
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-// app.use(passport.initialize());
-// app.use(passport.session());
+
 app.use(lusca.xframe('SAMEORIGIN'))
 app.use(lusca.xssProtection(true))
+
 app.use(passport.initialize())
 app.use(passport.session())
-
-// app.use((req, res, next) => {
-//     res.locals.user = req.user;
-//     next();
-// });
-
-// app.use((req, res, next) => {
-//     // After successful login, redirect back to the intended page
-//     if (!req.user &&
-//     req.path !== "/login" &&
-//     req.path !== "/signup" &&
-//     !req.path.match(/^\/auth/) &&
-//     !req.path.match(/\./)) {
-//         req.session.returnTo = req.path;
-//     } else if (req.user &&
-//     req.path == "/account") {
-//         req.session.returnTo = req.path;
-//     }
-//     next();
-// });
 
 /**
  * Primary app routes.
@@ -54,19 +33,5 @@ app.use('/auth', auth)
 
 // This endpoint must be the last one
 app.get('/', status(app))
-
-// /**
-//  * API examples routes.
-//  */
-// app.get("/api", apiController.getApi);
-// app.get("/api/facebook", passportConfig.isAuthenticated, passportConfig.isAuthorized, apiController.getFacebook);
-
-// /**
-//  * OAuth authentication routes. (Sign in)
-//  */
-// app.get("/auth/facebook", passport.authenticate("facebook", { scope: ["email", "public_profile"] }));
-// app.get("/auth/facebook/callback", passport.authenticate("facebook", { failureRedirect: "/login" }), (req, res) => {
-//     res.redirect(req.session.returnTo || "/");
-// });
 
 export default app
