294b917b357946baeada969293b4d9a6c14a760a
Alejandro U. Alvarez <aluralma@gmail.com>
2019-10-21 09:16:24 +0100

    complete deleting domains

--- dist/endpoints/domains/index.js
+++ dist/endpoints/domains/index.js
@@ -6,8 +6,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_promise_router_1 = __importDefault(require("express-promise-router"));
 const verify_1 = __importDefault(require("./verify"));
 const initVerification_1 = __importDefault(require("./initVerification"));
+const delete_1 = __importDefault(require("./delete"));
 exports.router = express_promise_router_1.default();
 exports.default = exports.router;
 exports.router.post('/verify/:domain', verify_1.default);
 exports.router.get('/verify/:domain', initVerification_1.default);
+exports.router.delete('/:uuid', delete_1.default);
 //# sourceMappingURL=index.js.map
--- src/endpoints/domains/index.ts
+++ src/endpoints/domains/index.ts
@@ -2,9 +2,11 @@ import Router from 'express-promise-router'
 
 import verify from './verify'
 import initVerification from './initVerification'
+import deleteDomain from './delete'
 
 export const router = Router()
 export default router
 
 router.post('/verify/:domain', verify)
 router.get('/verify/:domain', initVerification)
+router.delete('/:uuid', deleteDomain)
