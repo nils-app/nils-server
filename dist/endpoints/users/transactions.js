225f3c4100fe20e588da5adc40b84974f6fb947e
Alejandro U. Alvarez <aluralma@gmail.com>
2019-11-15 19:47:14 +0000

    return transaction uuid

--- dist/endpoints/users/transactions.js
+++ dist/endpoints/users/transactions.js
@@ -14,7 +14,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const db_1 = __importDefault(require("../../db"));
 exports.default = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const data = yield db_1.default.query('SELECT domains.domain, transactions.amount_nils, transactions.created_on FROM transactions LEFT JOIN domains ON domains.uuid = transactions.domain_id WHERE transactions.user_id = $1 LIMIT 100', [req.user.uuid]);
+    const data = yield db_1.default.query('SELECT transactions.uuid, domains.domain, transactions.amount_nils, transactions.created_on FROM transactions LEFT JOIN domains ON domains.uuid = transactions.domain_id WHERE transactions.user_id = $1 LIMIT 100', [req.user.uuid]);
     res.json(data.rows);
 });
 //# sourceMappingURL=transactions.js.map
--- src/endpoints/users/transactions.ts
+++ src/endpoints/users/transactions.ts
@@ -3,6 +3,6 @@ import { Response, Request } from 'express'
 import db from '../../db';
 
 export default async (req: Request, res: Response) => {
-  const data = await db.query('SELECT domains.domain, transactions.amount_nils, transactions.created_on FROM transactions LEFT JOIN domains ON domains.uuid = transactions.domain_id WHERE transactions.user_id = $1 LIMIT 100', [req.user.uuid]);
+  const data = await db.query('SELECT transactions.uuid, domains.domain, transactions.amount_nils, transactions.created_on FROM transactions LEFT JOIN domains ON domains.uuid = transactions.domain_id WHERE transactions.user_id = $1 LIMIT 100', [req.user.uuid]);
   res.json(data.rows)
 }
